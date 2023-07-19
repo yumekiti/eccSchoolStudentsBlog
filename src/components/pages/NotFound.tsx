77445f1006a2c20e3c65fb95bdfd01266bff8426
yumekiti <yumekiti120440@gmail.com>
2023-07-19 17:25:18 +0900

    feat: 404 page

--- src/components/pages/NotFound.tsx
+++ src/components/pages/NotFound.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import NotFoundIcon from './../../assets/NotFoundIcon';
 
 const NotFound = () => {
   return (
-    <div>
-      <h1>404: Not Found</h1>
+    <div className="flex flex-col items-center justify-center h-screen bg-blue-200">
+      <h1 className="text-blue-500 text-5xl font-bold mb-4">404: Not Found</h1>
+      <div className="flex items-center justify-center">
+        <NotFoundIcon />
+      </div>
     </div>
   );
 };
