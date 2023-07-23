81b90825c2c9ad856ae5fac37880583ca95b3a65
yumekiti <yumekiti120440@gmail.com>
2023-07-23 22:12:53 +0900

    feat: header design

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,19 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    colors: {
+      Background: '#d8eefe',
+      Headline: '#094067',
+      SubHeadline: '#5f6c7b',
+      CardBackground: '#fffffe',
+      CardHeading: '#094067',
+      CardParagraph: '#5f6c7b',
+      Stroke: '#094067',
+      Main: '#fffffe',
+      Highlight: '#3da9fc',
+      Secondary: '#90b4ce',
+      Tertiary: '#ef4565'
+    },
     extend: {},
   },
   plugins: [],
