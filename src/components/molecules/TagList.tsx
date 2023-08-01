300ab2b4d9c8b155e22e81a1398c7bcf832d1194
yumekiti <yumekiti120440@gmail.com>
2023-08-02 03:26:56 +0900

    fix: taglist

--- src/components/molecules/TagList.tsx
+++ src/components/molecules/TagList.tsx
@@ -5,11 +5,11 @@ type Props = {
 };
 
 const Component: React.FC<Props> = ({ tags }) => (
-  <ul className="flex items-center flex-wrap space-x-1 ml-2">
+  <ul className="flex items-center flex-wrap ml-2">
     {tags.map((tag, i) => (
       <li
         key={i}
-        className="text-SubHeadline hover:underline hover:bg-SubHeadline hover:bg-opacity-10 rounded p-1 text-xs md:text-sm hover:outline outline-1 outline-SubHeadline"
+        className="mr-1 text-SubHeadline hover:underline hover:bg-SubHeadline hover:bg-opacity-10 rounded p-1 text-xs md:text-sm hover:outline outline-1 outline-SubHeadline"
       >
         #{tag}
       </li>
