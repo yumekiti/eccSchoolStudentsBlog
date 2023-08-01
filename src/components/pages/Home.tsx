9e8c6ba468abd048f785dba0edcbae0fdcbd67cc
yumekiti <yumekiti120440@gmail.com>
2023-08-02 03:20:36 +0900

    feat: add NavigationMenu

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 
-import NavigationList from '../molecules/NavigationList';
-import CategoryList from '../molecules/CategoryList';
-
 import About from '../organisms/About';
 import RelatedLinks from '../organisms/RelatedLinks';
 import BlogList from '../organisms/BlogList';
+import NavigationMenu from '../organisms/NavigationMenu';
 
 import Layout from '../templates/Layout';
 
@@ -16,8 +14,7 @@ const Component = () => {
         <div className="max-w-7xl mx-auto">
           <div className="grid grid-cols-12 gap-4 px-2">
             <div className="hidden md:block md:col-span-3 lg:col-span-2">
-              <NavigationList />
-              <CategoryList />
+              <NavigationMenu />
             </div>
             <div className="col-span-12 md:col-span-9 lg:col-span-7">
               <BlogList />
