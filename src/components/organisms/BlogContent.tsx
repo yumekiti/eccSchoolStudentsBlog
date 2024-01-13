573d7b1e80c5effb563f849d10ca0e122a728e60
yumekiti <yumekiti120440@gmail.com>
2024-01-13 12:07:15 +0900

    feat: added highlights to the table

--- src/components/organisms/BlogContent.tsx
+++ src/components/organisms/BlogContent.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
 import rehypeRaw from 'rehype-raw';
@@ -6,6 +6,7 @@ import rehypeRaw from 'rehype-raw';
 import BlogTagList from '../molecules/BlogTagList';
 import BlogInfo from '../molecules/BlogInfo';
 import CodeBlock from '../molecules/CodeBlock';
+import { formatTableDate, formatTableTime } from '../../utils/format';
 
 type Props = {
   id: string;
@@ -27,55 +28,66 @@ const Component: React.FC<Props> = ({
   updated_at,
   title,
   content,
-}) => (
-  <div className="py-2">
-    <div className="bg-Main p-6 md:px-16 rounded">
-      <BlogInfo
-        user_id={user_id}
-        user_name={user_name}
-        user_image={user_image}
-        updated_at={updated_at}
-        created_at={created_at}
-      />
-      <div className="mt-2">
-        <h2 className="text-2xl font-bold text-Headline py-2 md:text-3xl">
-          {title}
-        </h2>
-        <div className="flex flex-col">
-          <BlogTagList id={id} />
+}) => {
+  const [formattedContent, setFormattedContent] = React.useState<string>('');
+
+  useEffect(() => {
+    let formatted = '';
+    formatted = formatTableDate(content);
+    formatted = formatTableTime(formatted);
+    setFormattedContent(formatted);
+  }, [content]);
+
+  return (
+    <div className="py-2">
+      <div className="bg-Main p-6 md:px-16 rounded">
+        <BlogInfo
+          user_id={user_id}
+          user_name={user_name}
+          user_image={user_image}
+          updated_at={updated_at}
+          created_at={created_at}
+        />
+        <div className="mt-2">
+          <h2 className="text-2xl font-bold text-Headline py-2 md:text-3xl">
+            {title}
+          </h2>
+          <div className="flex flex-col">
+            <BlogTagList id={id} />
+          </div>
+        </div>
+        <div className="markdown-body py-12">
+          <ReactMarkdown
+            remarkPlugins={[remarkGfm]}
+            rehypePlugins={[rehypeRaw]}
+            components={{
+              h1: ({ node, ...props }) => (
+                <h1 id={node.position?.start.line.toString()} {...props} />
+              ),
+              h2: ({ node, ...props }) => (
+                <h2 id={node.position?.start.line.toString()} {...props} />
+              ),
+              h3: ({ node, ...props }) => (
+                <h3 id={node.position?.start.line.toString()} {...props} />
+              ),
+              h4: ({ node, ...props }) => (
+                <h4 id={node.position?.start.line.toString()} {...props} />
+              ),
+              h5: ({ node, ...props }) => (
+                <h5 id={node.position?.start.line.toString()} {...props} />
+              ),
+              h6: ({ node, ...props }) => (
+                <h6 id={node.position?.start.line.toString()} {...props} />
+              ),
+              code: CodeBlock,
+            }}
+          >
+            {formattedContent}
+          </ReactMarkdown>
         </div>
       </div>
-      <div className="markdown-body py-12">
-        <ReactMarkdown
-          remarkPlugins={[remarkGfm]}
-          rehypePlugins={[rehypeRaw]}
-          components={{
-            h1: ({ node, ...props }) => (
-              <h1 id={node.position?.start.line.toString()} {...props} />
-            ),
-            h2: ({ node, ...props }) => (
-              <h2 id={node.position?.start.line.toString()} {...props} />
-            ),
-            h3: ({ node, ...props }) => (
-              <h3 id={node.position?.start.line.toString()} {...props} />
-            ),
-            h4: ({ node, ...props }) => (
-              <h4 id={node.position?.start.line.toString()} {...props} />
-            ),
-            h5: ({ node, ...props }) => (
-              <h5 id={node.position?.start.line.toString()} {...props} />
-            ),
-            h6: ({ node, ...props }) => (
-              <h6 id={node.position?.start.line.toString()} {...props} />
-            ),
-            code: CodeBlock,
-          }}
-        >
-          {content}
-        </ReactMarkdown>
-      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Component;
--- src/utils/format.ts
+++ src/utils/format.ts
@@ -6,3 +6,100 @@ export const getTitle = (path: string) => {
 
   return title;
 };
+
+export const formatTableDate = (content: string) => {
+  const contentArray = content.split('\n');
+  const today = new Date();
+  const day = today.getDate();
+
+  let isFormat = false;
+  let isStart = false;
+  let isEnd = false;
+
+  // date_ で始まっていたら、tabeleに入っている今日の日付をわかりやすく表示する
+  const formattedContentArray = contentArray.map((line) => {
+    const isDateHeading =
+      line.startsWith('## date_') || line.startsWith('### date_');
+
+    if (isDateHeading) {
+      // 今年の日付の場合、isHeadingをtrueに設定
+      const date = line.replace('date_', '').split(' ')[1];
+      const isCurrentYear =
+        date.split('年')[0] === today.getFullYear().toString();
+
+      line = line.replace('date_', '');
+
+      isFormat = isCurrentYear;
+    } else if (line.startsWith('##') || line.startsWith('###')) {
+      isFormat = false;
+    }
+
+    if (isFormat) {
+      // 全く同じ日付の場合、今日の日付をわかりやすく表示する
+      const splitedLine = line.split('|');
+      const splitedDay = splitedLine.slice(1, -1);
+      splitedDay.forEach((day, index) => {
+        splitedDay[index] = day.trim();
+      });
+      splitedDay.forEach((item, index) => {
+        if (item === '1') {
+          isStart = true;
+        }
+        if (isStart && String(day) === item && !isEnd) {
+          splitedDay[index] = `**${item}**`;
+          isEnd = true;
+        }
+        line = line.replace(splitedLine[index + 1], splitedDay[index]);
+      });
+    }
+
+    return line;
+  });
+
+  return formattedContentArray.join('\n');
+};
+
+export const formatTableTime = (content: string) => {
+  const contentArray = content.split('\n');
+
+  let isFormat = false;
+
+  // time_ で始まっていたら、tabeleに入っている今日の日付をわかりやすく表示する
+  const formattedContentArray = contentArray.map((line) => {
+    const isTimeHeading =
+      line.startsWith('## time_') || line.startsWith('### time_');
+
+    if (isTimeHeading) {
+      line = line.replace('time_', '');
+      isFormat = true;
+    } else if (line.startsWith('##') || line.startsWith('###')) {
+      isFormat = false;
+    }
+
+    if (isFormat) {
+      // :を含むか
+      const isTime = line.indexOf(':') !== -1;
+
+      if (isTime) {
+        // 17:00 ~ 19:50 などの場合、今の時間の範囲をわかりやすく表示する
+        const timeRange = line.split('~');
+        const startTime = timeRange[0].replace(' ', '').replace('|', '').trim(); // 17:00
+        const endTime = timeRange[1].replace(' ', '').replace('|', '').trim(); // 19:50
+
+        const now = new Date();
+        const nowTime = `${now.getHours()}:${now.getMinutes()}`;
+
+        if (startTime <= nowTime && nowTime <= endTime) {
+          line = line.replace(
+            `${startTime} ~ ${endTime}`,
+            `**${startTime} ~ ${endTime}**`,
+          );
+        }
+      }
+    }
+
+    return line;
+  });
+
+  return formattedContentArray.join('\n');
+};
